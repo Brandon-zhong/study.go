8f417ac998b276ad640ed804ef14b9306e1c9542
brandon <brandon-zhong8859@foxmail.com>
2021-01-24 23:48:12 +0800

    增加下载B站视频

--- bilibili_demo/main.go
+++ bilibili_demo/main.go
@@ -1,31 +1,59 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
 	"github.com/panjf2000/ants/v2"
+	"github.com/tidwall/gjson"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
+	"sync"
+	"time"
 )
 
 var pool, _ = ants.NewPool(100)
 
 func main() {
+	/*videoInfo := VideoInfo{
+		aid:      70211798,
+		cid:      223418336,
+		title:    "【2021版】2.1.2_BCD码",
+		page:     9,
+		baseUrl:  "https://www.bilibili.com/video/av70211798",
+		quality:  80,
+		filePath: "E:\\nfs\\download",
+	}
+	videoInfo := VideoInfo{
+		aid:      498807073,
+		cid:      209502920,
+		title:    "克隆史（上）",
+		page:     9,
+		baseUrl:  "https://www.bilibili.com/video/av498807073",
+		quality:  80,
+		filePath: "E:\\nfs\\download",
+	}
+	getPlayUrl(&videoInfo)
+	downloadVideo(&videoInfo)*/
 
+	startDownload("BV1X7411b7Yc")
 }
 
 type VideoInfo struct {
-	aid     int    //视频id
-	cid     int    //视频单P id
-	title   string //单P 的标题
-	page    int    //单P 号
-	referer string //一定要加
+	aid      int64    //视频id
+	cid      int64    //视频单P id
+	title    string   //单P 的标题
+	page     int      //单P 号
+	baseUrl  string   //一定要加, 基础url
+	quality  int      //视频质量 1080p:80;720p:64;480p:32;360p:16
+	filePath string   //视频下载的地址，只是目录地址
+	urlList  []string //视频的url地址
 }
-
-
 
 func genCheckRedirectFun(referer string) func(req *http.Request, via []*http.Request) error {
 	return func(req *http.Request, via []*http.Request) error {
@@ -34,80 +62,176 @@ func genCheckRedirectFun(referer string) func(req *http.Request, via []*http.Req
 	}
 }
 
-func download() {
-	referer := "https://api.bilibili.com/x/web-interface/view?aid=245576115/?p=1"
-	url := "http://upos-sz-mirrorcos.bilivideo.com/upgcxcode/26/05/262640526/262640526-1-80.flv?e=ig8euxZM2rNcNbR1hbUVhoM1hWNBhwdEto8g5X10ugNcXBlqNxHxNEVE5XREto8KqJZHUa6m5J0SqE85tZvEuENvNC8xNEVE9EKE9IMvXBvE2ENvNCImNEVEK9GVqJIwqa80WXIekXRE9IMvXBvEuENvNCImNEVEua6m2jIxux0CkF6s2JZv5x0DQJZY2F8SkXKE9IB5QK==&deadline=1611311779&gen=playurl&nbs=1&oi=3230711203&os=cosbv&platform=pc&trid=8c48d764e4b247a9ab3e504fc911b1f0&uipk=5&upsig=d82aff5dd460cf2db4a5bcdfd09d6160&uparams=e,deadline,gen,nbs,oi,os,platform,trid,uipk&mid=0"
-	cid := 262640526
-	var aid int64 = 245576115
-	title := "王道训练营-C语言教程"
-	page := 25
-	order := 1
+const entropy = "rbMCKn@KuamXWlPMoJGsKcbiJKUfkPF_8dABscJntvqhRSETg"
+const playUrlApi = "https://interface.bilibili.com/v2/playurl?%s&sign=%s"
+const paramsTmp = "appkey=%s&cid=%s&otype=json&qn=%s&quality=%s&type="
 
-	client := http.Client{CheckRedirect: genCheckRedirectFun(referer)}
-
-	request, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		log.Fatalln(url, err)
-		return
+func GetAppKey(entropy string) (appkey, sec string) {
+	revEntropy := ReverseRunes([]rune(entropy))
+	for i := range revEntropy {
+		revEntropy[i] = revEntropy[i] + 2
 	}
-	request.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:56.0) Gecko/20100101 Firefox/56.0")
-	request.Header.Set("Accept", "*/*")
-	request.Header.Set("Accept-Language", "en-US,en;q=0.5")
-	request.Header.Set("Accept-Encoding", "gzip, deflate, br")
-	request.Header.Set("Range", "bytes=0-")
-	request.Header.Set("Referer", referer)
-	request.Header.Set("Origin", "https://www.bilibili.com")
-	request.Header.Set("Connection", "keep-alive")
+	ret := strings.Split(string(revEntropy), ":")
 
-	resp, err := client.Do(request)
-	if err != nil {
-		log.Fatalf("Fail to download the video %d,err is %s", cid, err)
-		return
+	return ret[0], ret[1]
+}
+func ReverseRunes(runes []rune) []rune {
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
 	}
+	return runes
+}
 
-	if resp.StatusCode != http.StatusPartialContent {
-		log.Fatalf("Fail to download the video %d,status code is %d", cid, resp.StatusCode)
+func startDownload(videoId string) {
+
+	startTime := time.Now().Unix()
+	filder := "E:\\nfs\\download"
+	quality := 80
+
+	params := "aid=" + videoId
+	if strings.HasPrefix(videoId, "BV") {
+		params = "bvid=" + videoId
+	}
+	//获取视频列表
+	baseUrl := fmt.Sprintf("https://api.bilibili.com/x/web-interface/view?%s", params)
+	request, _ := http.NewRequest("GET", baseUrl, nil)
+	request.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36")
+	resp, err := http.DefaultClient.Do(request)
+	if err != nil {
+		log.Printf("get base url error, err --> %s", err)
 		return
 	}
 	defer resp.Body.Close()
+	all, _ := ioutil.ReadAll(resp.Body)
+	data := gjson.GetBytes(all, "data")
+	aid := data.Get("aid").Int()
+	filder = filepath.Join(filder, data.Get("title").String())
+	array := data.Get("pages").Array()
+	//开始下载，生成16个协程的协程池
+	pool, _ := ants.NewPool(32)
+	videoSize := len(array)
+	videoInfoChan := make(chan VideoInfo)
+	go func() {
+		for i := range array {
+			j := i
+			_ = pool.Submit(func() {
+				//封装下载信息
+				videoInfo := VideoInfo{
+					aid:      aid,
+					cid:      array[j].Get("cid").Int(),
+					title:    array[j].Get("part").String(),
+					page:     int(array[j].Get("page").Int()),
+					baseUrl:  baseUrl,
+					quality:  quality,
+					filePath: filder,
+				}
+				getPlayUrl(&videoInfo)
+				fmt.Println("get play url --> " + videoInfo.title)
+				videoInfoChan <- videoInfo
+			})
+		}
+	}()
 
-	aidPath := GetAidFileDownloadDir(aid, title)
-	filename := fmt.Sprintf("%d_%d.flv", page, order)
-	file, err := os.Create(filepath.Join(aidPath, filename))
-	if err != nil {
-		log.Fatalln(err)
-		os.Exit(1)
+	var waitGroup sync.WaitGroup
+	waitGroup.Add(videoSize)
+	for i := 0; i < videoSize; i++ {
+		_ = pool.Submit(func() {
+			defer waitGroup.Done()
+			videoInfo := <-videoInfoChan
+			downloadVideo(&videoInfo)
+		})
 	}
-	defer file.Close()
+	waitGroup.Wait()
+	log.Printf("all video has finished, spend time --> %s.", resolveTime(int(time.Now().Unix()-startTime)))
 
-	log.Println(title + ":" + filename + " is downloading.")
-	_, err = io.Copy(file, resp.Body)
-	if err != nil {
-		log.Printf("Failed to download video %d", cid)
-		return
-	}
-	log.Println(title + ":" + filename + " has finished.")
 }
 
-func GetAidFileDownloadDir(aid int64, title string) string {
-	curDir, err := os.Getwd()
-	if err != nil {
-		panic(err)
+func resolveTime(spendTime int) string {
+	second := spendTime % 60
+	minute := spendTime / 60
+	if minute == 0 {
+		return fmt.Sprintf("%ds", second)
 	}
-	title = strings.Replace(title, ":", "", -1)
-	title = strings.Replace(title, "\\", "", -1)
-	title = strings.Replace(title, "/", "", -1)
-	title = strings.Replace(title, "*", "", -1)
-	title = strings.Replace(title, "?", "", -1)
-	title = strings.Replace(title, "\"", "", -1)
-	title = strings.Replace(title, "<", "", -1)
-	title = strings.Replace(title, ">", "", -1)
-	title = strings.Replace(title, "|", "", -1)
-	// remove special symbal
-	fullDirPath := filepath.Join(curDir, "download", fmt.Sprintf("%d_%s", aid, title))
-	err = os.MkdirAll(fullDirPath, 0777)
-	if err != nil {
-		panic(err)
+	if minute < 60 {
+		return fmt.Sprintf("%dm%ds", minute, second)
 	}
-	return fullDirPath
+	hour := minute / 60
+	return fmt.Sprintf("%dh%dm%ds", hour, minute, second)
+}
+
+//获取是的播放地址
+func getPlayUrl(videoInfo *VideoInfo) {
+	//获取appKey和secret
+	key, sec := GetAppKey(entropy)
+	//生成参数和校验和
+	quality := strconv.Itoa(videoInfo.quality)
+	params := fmt.Sprintf(paramsTmp, key, strconv.FormatInt(videoInfo.cid, 10), quality, quality)
+	checkSum := fmt.Sprintf("%x", md5.Sum([]byte(params+sec)))
+	urlApi := fmt.Sprintf(playUrlApi, params, checkSum)
+
+	request, _ := http.NewRequest("GET", urlApi, nil)
+	request.Header.Set("Referer", videoInfo.baseUrl)
+	request.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36")
+
+	resp, err := http.DefaultClient.Do(request)
+	if err != nil {
+		log.Fatalf("get play url error, err --> %s,  status code --> %d", err, resp.StatusCode)
+		return
+	}
+	all, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("read respond fail, error --> %s", err)
+		return
+	}
+	var videoList []string
+	for _, i := range gjson.GetBytes(all, "durl").Array() {
+		videoList = append(videoList, i.Get("url").String())
+	}
+	videoInfo.urlList = videoList
+}
+
+func downloadVideo(videoInfo *VideoInfo) {
+	urlSize := len(videoInfo.urlList)
+	//当视频和多视频下载
+	if urlSize == 0 {
+		return
+	}
+	for index, url := range videoInfo.urlList {
+		request, _ := http.NewRequest("GET", url, nil)
+		request.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:56.0) Gecko/20100101 Firefox/56.0")
+		request.Header.Set("Accept", "*/*")
+		request.Header.Set("Accept-Language", "en-US,en;q=0.5")
+		request.Header.Set("Accept-Encoding", "gzip, deflate, br")
+		request.Header.Set("Range", "bytes=0-")
+		request.Header.Set("Referer", videoInfo.baseUrl)
+		request.Header.Set("Origin", "https://www.bilibili.com")
+		request.Header.Set("Connection", "keep-alive")
+		resp, err := http.DefaultClient.Do(request)
+		if err != nil {
+			log.Printf("get play url error, err --> %s", err)
+			continue
+		}
+		fmt.Println("文件大小：" + strconv.FormatInt(resp.ContentLength, 10))
+		//生成完整的文件目录
+		fileName := videoInfo.title
+		if urlSize != 1 {
+			fileName = videoInfo.title + "-" + strconv.Itoa(index)
+		}
+		filePath := filepath.Join(videoInfo.filePath, fileName+".mp4")
+		_ = os.MkdirAll(videoInfo.filePath, 0777)
+		file, err := os.Create(filePath)
+		if err != nil {
+			log.Printf("open file error, err --> %s", err)
+			continue
+		}
+		log.Println(fileName + " is downloading.")
+		if _, err = io.Copy(file, resp.Body); err != nil {
+			log.Printf("download video error, err --> %s", err)
+			continue
+		}
+		log.Println(fileName + " has finished.")
+		resp.Body.Close()
+		_ = file.Close()
+	}
+
 }
--- go.mod
+++ go.mod
@@ -13,14 +13,18 @@ require (
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/olivere/elastic/v7 v7.0.22
 	github.com/otiai10/gosseract/v2 v2.2.4
+	github.com/panjf2000/ants/v2 v2.4.0
 	github.com/satori/go.uuid v1.2.0
 	github.com/spf13/viper v1.6.2
+	github.com/tidwall/gjson v1.1.4
+	github.com/tidwall/match v1.0.3 // indirect
 	github.com/tsenart/vegeta/v12 v12.8.4
-	gitlab.gaeamobile-inc.net/sp2/gaeaspgo v1.2.0
+	golang.org/x/mobile v0.0.0-20201217150744-e6ae53a27f4f
+	// gitlab.gaeamobile-inc.net/sp2/gaeaspgo v1.2.0
 	golang.org/x/sys v0.0.0-20200720211630-cb9d2d5c5666 // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 	gorm.io/driver/mysql v1.0.1
 	gorm.io/gorm v1.20.0
-	github.com/panjf2000/ants/v2 v2.4.0
+
 )
